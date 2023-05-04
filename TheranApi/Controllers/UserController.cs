9c5a0d19ae954fcf272086a66b4698bbfcb7552d
Boris Sea <seaborisrc@gmail.com>
2023-05-03 20:18:51 -0400

    Start Demo controller

---
+++ TheranApi/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheranApi.Controllers
+{
+    [ApiController]
+    [Route("api/v2/[controller]")]
+    public class UserController : ControllerBase
+    {
+        public List<string> Users = new() { "Samuel", "Noemi", "Franz" };
+
+        // https://localhost:7055/api/v2/User/2
+        [HttpGet("{id}")]
+        public string GetUsersWithIdRequired(int id)
+        {
+            if(Users.Count >= id)
+                return Users.ElementAt(id - 1);
+            return "No hay el usuario";
+        }
+
+        // https://localhost:7055/api/v2/User?name=Franz
+        [HttpGet]
+        public string ExistUsersByName(string name)
+        {
+           var result =  Users.Find(names => names == name);
+
+            if (result == null)
+                return "No hay el nombre";
+
+            return "El nombre si existe";
+        }
+
+    }
+}
